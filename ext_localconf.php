d002e8156d9a9c54091d33a54206d738ae096ead
Marc Bastian Heinrichs <typo3@mbh-software.de>
2018-03-16 10:01:06 +0100

    [TASK] Use SYS Objects instead Container::registerImplementation

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,8 +5,8 @@ if (!defined('TYPO3_MODE')) {
 
 use MbhSoftware\SolrFluidIndexer\IndexQueue\FrontendHelper\PageFieldFluidIndexer;
 
-$extbaseObjectContainer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Extbase\Object\Container\Container::class);
-$extbaseObjectContainer->registerImplementation(\TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder::class, \MbhSoftware\SolrFluidIndexer\Mvc\Web\Routing\UriBuilder::class);
-unset($extbaseObjectContainer);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder::class] = [
+    'className' => \MbhSoftware\SolrFluidIndexer\Mvc\Web\Routing\UriBuilder::class
+];
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['solr']['Indexer']['indexPageSubstitutePageDocument'][PageFieldFluidIndexer::class] = PageFieldFluidIndexer::class;
