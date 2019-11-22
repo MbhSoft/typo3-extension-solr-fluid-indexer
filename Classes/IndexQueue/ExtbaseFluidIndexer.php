39bf2180263f7aa3cd1b2630896b1ddd98cf2159
Marc Bastian Heinrichs <typo3@mbh-software.de>
2019-11-22 09:26:34 +0100

    [BUGFIX] Wrong if expression

--- Classes/IndexQueue/ExtbaseFluidIndexer.php
+++ Classes/IndexQueue/ExtbaseFluidIndexer.php
@@ -83,7 +83,7 @@ class ExtbaseFluidIndexer extends \ApacheSolrForTypo3\Solr\IndexQueue\Indexer
     {
         $document = parent::itemToDocument($item, $language);
 
-        if (!$document !== null) {
+        if ($document !== null) {
             $this->cObj = $GLOBALS['TSFE']->cObj;
             $indexingConfiguration = $this->getItemTypeAllConfiguration($item, $language);
 
@@ -97,7 +97,7 @@ class ExtbaseFluidIndexer extends \ApacheSolrForTypo3\Solr\IndexQueue\Indexer
                 $objectNameInFluid = $item->getIndexingConfigurationName();
             }
 
-            if (!is_null($object)) {
+            if ($object !== null) {
                 $this->initializeStandaloneView($indexingConfiguration['template.']);
                 $this->view->assign($objectNameInFluid, $object);
                 if (isset($indexingConfiguration['fieldsFromSections.'])) {
